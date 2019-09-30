0c28fcf516cf5ee2198b49987e332317f1d5fc0a
dongchul <sah3122@naver.com>
2019-09-30 13:11:13 +0900

    Project setting

---
+++ src/main/java/me/study/studyjpashop/StudyJpaShopApplication.java
@@ -0,0 +1,13 @@
+package me.study.studyjpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJpaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyJpaShopApplication.class, args);
+    }
+
+}
