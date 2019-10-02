cdd744dab0dd4e8bdec23a8add9054ed7f6b1a87
dongchul <sah3122@naver.com>
2019-10-02 16:38:49 +0900

    MAKE cascade option & relation method

--- src/main/java/me/study/studyjpashop/domain/Order.java
+++ src/main/java/me/study/studyjpashop/domain/Order.java
@@ -21,10 +21,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL) // cascade는 persist를 전파 한다. 따라서 order를 저장하면 변경된 orderitem이 저장된다.
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +32,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+    //연관관계 메서드 양방향 연관관계일때 사용하면 편리함.
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
