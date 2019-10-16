50b853957e3325205244725a9439c013382ea1c9
dongchul <sah3122@naver.com>
2019-10-16 11:22:41 +0900

    ADD Member Create

---
+++ src/main/java/me/study/studyjpashop/controller/MemberController.java
@@ -0,0 +1,44 @@
+package me.study.studyjpashop.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.study.studyjpashop.domain.Address;
+import me.study.studyjpashop.domain.Member;
+import me.study.studyjpashop.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) {
+
+        if(result.hasErrors()) { // BindingResult는 Valid 에서 에러가 날 경우 해당 에러를 result 에 담아둔다.
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
